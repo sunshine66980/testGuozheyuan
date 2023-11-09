7fe26b75f82037246c4a5b185adeb678b2fef6d4
冲浪人 <12628805+renchonglang_12345@user.noreply.gitee.com>
2023-11-09 19:39:41 +0800

    删除多余文件

--- zzk/javeSE/d3/HelloJava/src/com/geek/test/GuessNum.java
+++ zzk/javeSE/d3/HelloJava/src/com/geek/test/GuessNum.java
@@ -1,14 +1,24 @@
 package com.geek.test;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNum {
+    private static int maxGuess = 3;
     public static void main(String[] args) {
-        int num = 76;
+        Random random = new Random();
+        int num = random.nextInt(100);
+        System.out.println(num);
         Scanner sc = new Scanner(System.in);
+        int count = 0;
         do {
+            if (count == maxGuess){
+                System.out.println("猜测次数用完.");
+                break;
+            }
             System.out.println("输入一个数");
             int guessNum = sc.nextInt();
+            count++;
             if (guessNum == num){
                 System.out.println("猜对了!");
                 break;
--- zzk/javeSE/d3/HelloJava/src/com/geek/test/WaterFlower.java
+++ zzk/javeSE/d3/HelloJava/src/com/geek/test/WaterFlower.java
@@ -4,10 +4,14 @@ public class WaterFlower {
     public static void main(String[] args) {
         System.out.println("水仙花数:");
         int count = 0;
-        for (int i = 1; i <9 ; i++) {
+        for (int i = 0; i <9 ; i++) {
             for (int j = 0; j < 9; j++) {
                 for (int k = 0; k < 9; k++) {
                     if (Math.pow(i,3) + Math.pow(j,3) + Math.pow(k,3) == i+j*10+k*100){
+//                        排除0和1
+                        if (j==0 && k==0 ){
+                            continue;
+                        }
                         System.out.println(i+j*10+k*100);
                         count++;
                     }
