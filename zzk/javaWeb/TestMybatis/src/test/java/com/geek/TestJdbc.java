168b49e357b0bc05ee29713c854c0d7793ed67f3
冲浪人 <12628805+renchonglang_12345@user.noreply.gitee.com>
2023-11-14 10:40:04 +0800

    测试类

--- zzk/javaWeb/TestMybatis/src/test/java/com/geek/TestJdbc.java
+++ zzk/javaWeb/TestMybatis/src/test/java/com/geek/TestJdbc.java
@@ -1,11 +1,16 @@
 package com.geek;
 
+import com.geekaca.demo.TestBrand;
+import com.geekaca.pojo.Brand;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class TestJdbc {
     @Test
-    public void testJdbc(){
-
+    public void testJdbc() throws Exception {
+        TestBrand brand = new TestBrand();
+        Brand brand1 =  TestBrand.testSelect(1000048);
+        Assert.assertNotNull(brand1);
     }
 
 }
